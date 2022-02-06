c44b2aa5df69bae47d6ade0b0890b7f77c44254a
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-02-06 11:44:10 +0100

    Implement bird movement

--- scripts/BirdMovement.gd
+++ scripts/BirdMovement.gd
@@ -1,24 +1,29 @@
 extends KinematicBody2D
 
-export var horizontal_speed = 3
-export var jump_speed = 1
-export var gravity = 10
+export var horizontal_speed = 5
+export var jump_speed = 7
+export var gravity = 20
+export var facing_right = true
 
 var time_since_jump = 0
 var jumping = false
 var velocity
 
+func handle_collision(collision: KinematicCollision2D):
+	facing_right = collision.collider.get("left_wall")
+	$Sprite.flip_h = not facing_right
+
 func _ready():
 	velocity = Vector2(horizontal_speed, -jump_speed)
 
 func _physics_process(delta):
-	# prevent infinite jumping
-	if Input.action_press("ui_up") and not jumping:
+	if Input.is_action_just_pressed("ui_up"):
 		velocity.y = -jump_speed
 		time_since_jump = 0
-	if Input.action_release("ui_up"):
-		jumping = false
 	
+	velocity.x = horizontal_speed if facing_right else -horizontal_speed
 	velocity.y += gravity * delta
 
-	move_and_collide(velocity)
+	var collision = move_and_collide(velocity)
+	if collision:
+		handle_collision(collision)
