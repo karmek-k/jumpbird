750071d979d09b00ec65a90eb281641bef15a15f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-02-08 18:01:00 +0100

    Finish the spike generation script

--- scripts/BirdMovement.gd
+++ scripts/BirdMovement.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
+
 signal game_over
+signal wall_bumped
 
 export var horizontal_speed = 5
 export var jump_speed = 7
@@ -11,13 +13,17 @@ var time_since_jump = 0
 var jumping = false
 var velocity
 
+
 func handle_collision(collision: KinematicCollision2D):
 	facing_right = collision.collider.get("left_wall")
 	$Sprite.flip_h = not facing_right
+	emit_signal("wall_bumped", facing_right)
+
 
 func _ready():
 	$Sprite.flip_h = not facing_right
 	velocity = Vector2(horizontal_speed, -jump_speed)
+
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up"):
@@ -30,8 +36,6 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity)
 	if collision:
 		handle_collision(collision)
-	if is_on_ceiling() or is_on_floor():
-		emit_signal("game_over")
 
 
 func _on_Spike_body_entered(body: Node):
--- scripts/Root.gd
+++ scripts/Root.gd
@@ -1,5 +1,57 @@
 extends Node2D
 
 
+export var spike_height = 40
+export var min_holes = 2
+export var max_holes = 5
+
+var spike_scene = preload("res://scenes/Spike.tscn")
+
+
+func _ready():
+	randomize()
+
+
 func _on_Bird_game_over():
 	get_tree().reload_current_scene()
+
+
+func _on_Bird_wall_bumped(facing_right: bool):
+	var viewport_height = get_viewport_rect().size.y
+	var max_spike_count = floor(viewport_height / spike_height)
+	var spike_positions = []
+
+	# delete previous spikes
+	for spike in $Spikes.get_children():
+		spike.queue_free()
+
+	# determine the wall the spikes should be placed on
+	var spike_start_pos
+	if facing_right:
+		spike_start_pos = $RSpikeStartPos
+	else:
+		spike_start_pos = $LSpikeStartPos
+
+	# build the array of possible spike y coords
+	for i in max_spike_count:
+		spike_positions.append(i * spike_height + spike_start_pos.position.y)
+
+	# generate the number of removed coords
+	var removed_count = randi() % (max_holes + 1)
+	while removed_count < min_holes:
+		removed_count = randi() % (max_holes + 1)
+
+	# remove coords
+	for _i in removed_count:
+		spike_positions.remove(randi() % spike_positions.size())
+
+	# place spikes
+	for spike_y in spike_positions:
+		var spike = spike_scene.instance()
+		spike.position.x = spike_start_pos.position.x
+		spike.position.y = spike_y
+		
+		if facing_right:
+			spike.rotation = PI
+		
+		$Spikes.add_child(spike)
